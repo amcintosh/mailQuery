ee356be3cba0c454a939d43626ef5d1c56e4194d
Andrew McIntosh <nacho.vonkickbutt@gmail.com>
2012-05-29 07:51:19 -0400

    Modified Config file to include various properties. These are also now loaded by the mailer.
    Next: Use param properties in query
    Then: Finish emailer

--- mailQuery.py
+++ mailQuery.py
@@ -5,10 +5,11 @@ DB_USER="andrew"
 DB_PASSWORD="andrew"
 DB_TNS="dev10g"
 
-import cx_Oracle
+#import cx_Oracle
 import sys
 import datetime
 import smtplib
+import ConfigParser
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
@@ -28,17 +29,47 @@ def usage():
     print "Usage: mailQuery <query_file> Optional: <user> <password> <tns_name> "
 
 
-'''Connect to Oracle database'''
 def getConnection(user=DB_USER,password=DB_PASSWORD,tns=DB_TNS):
+    '''Connect to Oracle database'''
     connectStr = user+"/"+password+"@"+tns
     return cx_Oracle.connect(connectStr)
 
 
-def getQueryFromFile(filepath):
-    infile = open(filepath, 'r')
-    query = infile.read()
-    infile.close()
-    return query
+def getQueryDataFromFile(filepath):
+    '''Read config file and parse all queries and their associated config.
+    Returns a list of dictionaries, where each dictionary is a separate 
+    query to run containing the parameters for that query.
+    '''
+    Config = ConfigParser.ConfigParser()
+    Config.read(filepath)
+    queryData = []
+    for section in Config.sections():
+        thisQuery = {}
+        for name,value in Config.items(section):
+            if name!="params":
+                thisQuery[name] = value
+            else:
+                thisQuery[name] = constructParams(value)
+        queryData.append(thisQuery)
+    return queryData
+
+
+def constructParams(paramStr):
+    '''Takes a string of parameters and constructs a list from them.
+       Date placeholders are replaced with date objects. All other 
+       strings are copied as is.
+    '''
+    params = paramStr.split(",")
+    newParams = []
+    for param in params:
+        if param.strip()=="FIRSTOFTHISMONTH":
+            pass
+        elif param.strip()=="FIRSTOFLASTMONTH":
+            pass
+        else:
+            newParams.append(param.strip())
+    return newParams
+
 
 def writeCsv(cursor,outfilename):
     outfile = open(outfilename, 'w')
@@ -53,7 +84,7 @@ def mailCsv(outfilename):
     fp = open(outfilename, 'rb')
     msg = MIMEMultipart()
     msg['From'] = "amcintosh"
-    msg['To'] = "amcintosh@otn.ca" #COMMASPACE.join(send_to)
+    msg['To'] = "amcintosh@something.com" #COMMASPACE.join(send_to)
     msg['Subject'] = "test"
     text = "This is my test"
     msg.attach( MIMEText(text) )
@@ -61,8 +92,8 @@ def mailCsv(outfilename):
     part.set_payload(fp.read())
     part.add_header('Content-Disposition', 'attachment', filename=outfilename)
     msg.attach(part)
-    smtp = smtplib.SMTP("exchange.otn.local",25)
-    smtp.sendmail("amcintosh@otn.ca", "amcintosh@otn.ca", msg.as_string())
+    smtp = smtplib.SMTP("server",25)
+    smtp.sendmail("amcintosh@isomething.com", "amcintosh@something.com", msg.as_string())
     smtp.close()
 
     
@@ -71,7 +102,7 @@ def main(argv):
         usage()
         sys.exit(2)
 
-    query = getQueryFromFile(argv[1])
+    query = getQueryDataFromFile(argv[1])
     print query
     if len(argv)>5:
         con = getConnection(argv[2],argv[3],argv[4])
@@ -80,10 +111,9 @@ def main(argv):
     cursor = con.cursor()
     cursor.arraysize = 256
     cursor.execute(query)
-
     outfilename = "output.txt"
     writeCsv(cursor,outfilename)
-    #mailCsv(outfilename)
+    mailCsv(outfilename)
     
     cursor.close()
     con.close()
